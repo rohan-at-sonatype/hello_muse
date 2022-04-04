67794fb66abc87e940533435dbe323dbb7db7c6e
rohanbhaumik <rbhaumik@sonatype.com>
2022-04-04 10:16:00 -0400

    Testing IDE setup.

--- main.c
+++ main.c
@@ -4,6 +4,7 @@ static char text[] = "Hello Sonatype Lift !";
 
 int main() {
     char * printme = text;
-    printf("%s\n",printme); 
+    printf("%s\n",printme);
+    // this is a comment I am making to test merging things into GitHub
     return 0;
 }
