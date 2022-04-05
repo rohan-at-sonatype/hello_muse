e325f9a3c0d9f12c93045d7a8fd48432016f559f
rohanbhaumik <rbhaumik@sonatype.com>
2022-04-05 16:58:45 -0400

    Committing to test_branch_3

--- main.c
+++ main.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 
-static char text[] = "Hello Sonatype Lift !";
+static char text[] = "Hello Rohan1234!";
 
 int main() {
     char * printme = text;
+    printme = NULL;
+    printme[0] = 'X';
+    printme[1] = 'X';
+    printme[2] = 'X';
+    printme[3] = '\0';
     printf("%s\n",printme);
     return 0;
+    //my name is Rohan Bhaumik and I rock - new PR on test_branch_3
 }
