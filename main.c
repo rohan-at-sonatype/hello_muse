2a133c6e29fb01ffbf57fd8b36a1cabeffb94bfe
rohanbhaumik <rbhaumik@sonatype.com>
2022-04-05 15:42:54 -0400

    Back to old

--- main.c
+++ main.c
@@ -1,15 +1,9 @@
 #include <stdio.h>
 
-static char text[] = "Hello Rohan!";
+static char text[] = "Hello Sonatype Lift !";
 
 int main() {
     char * printme = text;
-    printme = NULL;
-    printme[0] = 'X';
-    printme[1] = 'X';
-    printme[2] = 'X';
-    printme[3] = '\0';
     printf("%s\n",printme);
     return 0;
-    //my name is Rohan Bhaumik and I rock
 }
